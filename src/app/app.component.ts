8e2bee780d82d6d5504bcaca8c32b595ed4066fb
Arctroid1 <arctroidrobotmink@gmail.com>
2020-03-02 18:00:48 +0100

    Affichage des données de l'API GET 'https://arnaud.neonoos.com/places'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import {  HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ArnaudNeonoos';
+
+
+  places = [];
+  post = {
+    name: '',
+   description: '',
+   address: '',
+   lat: 0,
+   lng: 0,
+   rating: 0,
+}
+
+constructor(private http: HttpClient){
+
+  this.getData();
+   
+}
+
+
+// GET
+getData(){
+
+  let url =  "https://arnaud.neonoos.com/places";
+  let data : Observable<any> = this.http.get(url);
+  data.subscribe(result => {
+    
+    this.places = result.data;
+
+    console.log("listes des places");
+      console.log(result.data);
+      console.log(result.data.id);
+       
+  });
+}
+
+
+
 }
